496c06ba110baa8e3a3eb03a718584d47e96824d
md-shemil <shemil1625@gmail.com>
2025-06-25 15:35:54 +0530

    updating with cors

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ import connectDB from './config/db.js';
 import dotenv from 'dotenv';
 import userRouter from './routes/userRoutes.js';
 import taskRouter from './routes/taskRoute.js';
+import cors from 'cors';
 
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT ;
+
+app.use(cors());
 
 app.use(express.json());
 
